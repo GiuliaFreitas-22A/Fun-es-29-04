4ad3a16340a6d5daead3fcf316eb44fbffa1f639
GiuliaFreitas-22A <giulia.fsantos@eaportal.org>
2025-04-27 19:16:35 -0300

    Fim da atividade

--- main.js
+++ main.js
@@ -1,7 +1,98 @@
 // funções = bloco de código, usado para uma funcionalidade específica que só executado quando for chamado 
-
+/*
+## exemplo 1
 function mensagem() {
     alert("Seja bem-vindo(a)!")
+    console.log("Seja bem-vindo(a)!")
+    console.log("Você é muito legal!")
     }
     
-    mensagem()
+ mensagem()
+ */
+
+ /* 
+ ## exemplo 2
+ 
+ function somar(numero1, numero2) {
+    console.log(numero1 + numero2)
+ }
+
+ let numero1 = 40
+ let numero2 = 23
+
+ somar(numero1, numero2)
+ somar(30, 12)
+ somar(19, 289)
+ */
+
+
+ /*
+## exemplo 3
+
+ function raizQuadrada(numero) {
+    console.log(Math.sqrt(numero))
+ }
+
+ funcion potenciacao(valor1, valor2) {
+    console.log(Match.pow(valor1, valor2))
+ }
+
+ function multiplicacao(valor1, valor2) {
+    console.log(valor1 * valor2)
+ }
+
+ let numero1 = Number(prompt("Digite o número 1:"))
+ let numero1 = Number(prompt("Digite o número 2:"))
+
+ raizQuadrada(numero1)
+ potenciacao(numero1, numero2)
+ multiplicacao(numero2, numero1)
+
+ */
+
+ /* 
+ ## exemplo 4
+
+  function raizQuadrada(numero) {
+     return console.log(Math.sqrt(numero))
+ }
+
+ funcion potenciacao(valor1, valor2) {
+   return console.log(Match.pow(valor1, valor2))
+ }
+
+ function multiplicacao(valor1, valor2) {
+  return  console.log(valor1 * valor2)
+ }
+
+ let numero1 = Number(prompt("Digite o número 1:"))
+ let numero1 = Number(prompt("Digite o número 2:"))
+
+ let raiz1 = raizQuadrada(numero1)
+ let raiz2 = raizQuadrada(numero2)
+
+ console.log(raiz1, raiz2)
+
+ */
+
+ /*
+ ## exemplo 5
+
+ function boasVindas(nome, idade) {
+    return ´
+    Seja bem-vindo(a), &{nome}!
+    &{nome}, você é muito especial!
+    Parabéns pelo seu aniversário de &{idade} anos.
+    ´
+ }
+
+ let nome = prompt("Digite seu nome:")
+ let idade = prompt("Digite sua idade:")
+
+ let mensagem = boasVindas(nome, idade)
+
+ alert(mensagem)
+ console.log(mensagem)
+ */
+
+ 
