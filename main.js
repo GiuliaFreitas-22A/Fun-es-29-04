8e538335092ae46a43488c0793e9cc9f31b086ee
GiuliaFreitas-22A <giulia.fsantos@eaportal.org>
2025-04-27 17:59:06 -0300

    Inicio do projeto

---
+++ main.js
@@ -0,0 +1,7 @@
+// funções = bloco de código, usado para uma funcionalidade específica que só executado quando for chamado 
+
+function mensagem() {
+    alert("Seja bem-vindo(a)!")
+    }
+    
+    mensagem()
